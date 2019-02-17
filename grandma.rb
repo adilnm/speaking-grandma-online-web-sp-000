200c7da73fa6cf551aff577422cfd58ddb849212
ADIL NAIMI <adilnm23@hotmail.com>
2019-02-17 06:34:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,8 @@
 def speak_to_grandma(phrase)
   if(phrase==phrase.upcase)
     return "NO, NOT SINCE 1938!"
-  elsif(phrase==)
+  elsif(phrase=="I LOVE YOU GRANDMA!")
+  return 
   else 
     return "HUH?! SPEAK UP, SONNY!"
   end
